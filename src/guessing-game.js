efb6a513de6c30f0c94be11e2d865f31a351bf86
Andrei-Zhuk <zhuk_andrey@tut.by>
2017-02-02 14:55:33 +0300

    guessing-game: methods are implemented

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    	this._min = 0;
+    	this._max = 0;
+    	this._guess = 0;
+    }
 
     setRange(min, max) {
-
+    	this._min = min;
+    	this._max = max;
     }
 
     guess() {
-
+    	this._guess = Math.round((this._max + this._min)/2);
+    	return this._guess;
     }
 
     lower() {
-
+    	this._max = this._guess;
     }
 
     greater() {
-
+    	this._min = this._guess;
     }
 }
 
